2ee70fba4a35766c713e6018faaa463497033a66
yuraromaniuk <y.romaniuk1990@gmail.com>
2018-06-14 02:56:56 +0300

    first commit

---
+++ conf.js
@@ -0,0 +1,8 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js'],
+  capabilities: {
+    browserName: 'chrome'
+  }
+};
---
+++ todo-spec.js
@@ -0,0 +1,19 @@
+describe('angularjs homepage todo list', function() {
+  it('login on page', function() {
+    browser.waitForAngularEnabled(false);
+    browser.get('https://app.cms2cms.com/auth');
+
+    element(by.id('signUpEmail')).sendKeys('yura_test@gmail.com');
+
+    element(by.id('signUpPassword')).sendKeys('12344321qwe');
+    element(by.id('signUpName')).sendKeys('Yura Romanyuk');
+    element(by.id('signUpPhone')).sendKeys('(201) 555-0123');
+    element(by.id('agree-with-terms')).click();
+	
+    element(by.css('button.btn.btn-default')).click();
+
+	
+    element(by.className('user-logout'));
+
+  });
+});
